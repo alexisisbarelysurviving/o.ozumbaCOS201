aa48b0e5635ed0bcb7889766d0000fdc86ee2cce
unknown <ozumbaodera241@gmail.com>
2025-11-17 09:24:17 +0100

    all practices

---
+++ practice.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int a = 40;
+int b = 30;
+
+int main() 
+{
+   {
+       int a = 2;
+       int b = 4;
+       cout << a << endl;
+       cout << b << endl;
+   }
+       cout << a << endl;
+       cout << b << endl;
+       
+    return 0;
+}
